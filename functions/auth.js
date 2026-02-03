5f7d9a98c56dfabef47a35044c335840acd0fe48
kebo-sukses <73052720+kebo-sukses@users.noreply.github.com>
2026-02-03 10:26:34 +0700

    Fix auth.js - redirect to Netlify callback instead of luxiephoto.com

--- functions/auth.js
+++ functions/auth.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const client_id = process.env.OAUTH_CLIENT_ID;
-  // Use luxiephoto.com callback which is registered in GitHub OAuth
-  const redirect_uri = 'https://luxiephoto.com/api/auth/callback';
+  // Callback ke Netlify function sendiri
+  const redirect_uri = 'https://dancing-squirrel-226937.netlify.app/.netlify/functions/callback';
   
   const authUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,user&redirect_uri=${encodeURIComponent(redirect_uri)}`;
   
